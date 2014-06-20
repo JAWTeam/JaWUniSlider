509e91923f23226d0b358c9b7e159712b1acfa84
V.Pshentsov <pshentsoff@gmail.com>
2014-06-20 21:05:54 +0600

    0.0.5-dev camera jquery slideshow

--- mod_jawunislider/tmpl/default.php
+++ mod_jawunislider/tmpl/default.php
@@ -20,13 +20,15 @@
 defined('_JEXEC') or die;
 
 // Get params. You can set'em manually
+$slideShow          = $params->get('slideShow', 'camera');
+
 $moduleWidth		= $params->get('moduleWidth', '100%');
 $maxWidth			= $params->get('maxWidth', '100%');
 $moduleHeight		= $params->get('moduleHeight', '50%');
 $resizeImage		= $params->get('resizeImage', 1);
 $imageWidth			= $params->get('imageWidth', '600');
 $imageHeight		= $params->get('imageHeight', '300');
-$displayCaptions	= $params->get('displayCaptions', 1);
+$displayCaptions	= $params->get('displayCaptions', 0);
 
 $thumbnails			= $params->get('thumbnails', 1);
 $thumbnailWidth		= $params->get('thumbnailWidth', '100');
@@ -45,27 +47,39 @@ $doc = JFactory::getDocument();
 $doc->addScript('modules/mod_jawunislider/assets/jquery.mobile.customized.min.js', 'text/javascript');
 $doc->addScript('modules/mod_jawunislider/assets/jquery.easing.1.3.js', 'text/javascript');
 $doc->addScript('modules/mod_jawunislider/assets/jawunislider.js', 'text/javascript');
+
+switch($slideShow) {
+    case 'camera':
+    default: {
+        $doc->addScript('modules/mod_jawunislider/assets/camera.js', 'text/javascript');
+        $doc->addStyleSheet('modules/mod_jawunislider/assets/camera.css');
+
+        $slideShowWrapperClass = 'jaw-camera-slider';
+        $slideShowSliderClass = 'camera_wrap';
+        break;
+    }
+}
+
 $doc->addStyleSheet('modules/mod_jawunislider/assets/jawunislider.css');
 
 $timthumb = JURI::base() . 'modules/mod_jawunislider/libs/timthumb.php?a=c&q=99&z=0';
 
 ?>
 <style type="text/css">
-    <?php echo '#'.$domModuleWrapperId; ?> {
+    #<?php echo $domModuleWrapperId; ?> {
         width: <?php echo $moduleWidth; ?>;
         max-width: <?php echo $maxWidth; ?>;
         clear: both;
     }
 </style>
 
-<div id="<?php $domModuleWrapperId; ?>" class="jaw-slider-wrapper">
-    <div id="<?php $domModuleId; ?>" class="jaw-slider">
+<div id="<?php echo $domModuleWrapperId; ?>" class="<?php echo $slideShowWrapperClass; ?>">
+    <div id="<?php echo $domModuleId; ?>" class="<?php echo $slideShowSliderClass; ?>">
         <?php foreach($slides as $slide) : ?>
             <?php
+            $image = $slide->image;
             $bigImage   = $image;
-
             if($resizeImage) $bigImage = $timthumb . '&w=' . $imageWidth . '&h=' . $imageHeight . '&src=' . $image;
-
             $thumbImage = $timthumb . '&w=' . $thumbnailWidth . '&h=' . $thumbnailHeight . '&src=' . $image;
             ?>
             <div data-thumb="<?php echo $thumbImage; ?>" data-src="<?php echo $bigImage; ?>">
@@ -74,3 +88,34 @@ $timthumb = JURI::base() . 'modules/mod_jawunislider/libs/timthumb.php?a=c&q=99&
         <?php endforeach; ?>
     </div>
 </div>
+
+<script>
+    jQuery(document).ready(function ($) {
+<?php
+switch($slideShow) {
+    case 'camera':
+    default: {
+?>
+        jQuery('#<?php echo $domModuleWrapperId; ?>').camera({
+            loader				: '<?php echo $loaderStyle; ?>',
+            barDirection		: '<?php echo $barDirection; ?>',
+            barPosition			: '<?php echo $barPosition; ?>',
+            fx					: '<?php echo $fx; ?>',
+            piePosition			: '<?php echo $piePosition; ?>',
+            height				: '<?php echo $moduleHeight; ?>',
+            hover				: <?php echo ($pauseHover) ? 'true' : 'false'; ?>,
+            navigation			: <?php echo ($navigation) ? 'true' : 'false'; ?>,
+            navigationHover		: <?php echo ($navigationHover) ? 'true' : 'false'; ?>,
+            pagination			: <?php echo ($pagination) ? 'true' : 'false'; ?>,
+            playPause			: <?php echo ($playPause) ? 'true' : 'false'; ?>,
+            pauseOnClick		: <?php echo ($pauseOnClick) ? 'true' : 'false'; ?>,
+            thumbnails			: <?php echo ($thumbnails) ? 'true' : 'false'; ?>,
+            time				: <?php echo $duration; ?>,
+            transPeriod			: <?php echo $transPeriod; ?>
+        });
+<?php
+    }
+}
+?>
+    });
+</script>
