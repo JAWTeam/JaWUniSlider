dfb73e91d0abb28b1054d266d7a2ed587b498256
V.Pshentsov <pshentsoff@gmail.com>
2014-06-20 14:11:45 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+## Title
+
+Description
+
+-   Contributors: pshentsoff
+-   [Donate link](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=FGRFBSFEW5V3Y "Please, donate to support project")
+-   Tags: joomla, module, slider, jquery
+-   Author: pshentsoff
+-   [Author's homepage](http://pshentsoff.ru "Author's homepage")
+-   License: Apache License, Version 2.0
+-   License URI: http://www.apache.org/licenses/LICENSE-2.0.html
+
+### Useful links:
+
+#### Version 0.0.1-dev
+#### Last work version 0.0.1
+#### Last stable version 0.0.1
+
+### Versions history
+
+#### version 0.0.1-dev
---
+++ mod_jawunislider/helper.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @file        helper.php
+ * @description
+ *
+ * PHP Version  5.3.13
+ *
+ * @package 
+ * @category
+ * @plugin URI
+ * @copyright   2014, Vadim Pshentsov. All Rights Reserved.
+ * @license     http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ * @author      Vadim Pshentsov <pshentsoff@gmail.com> 
+ * @link        http://pshentsoff.ru Author's homepage
+ * @link        http://blog.pshentsoff.ru Author's blog
+ *
+ * @created     20.06.14
+ */
+
+defined('_JEXEC') or die;
+
+class modJaWUniSliderHelper {
+
+    public function getSlides() {
+
+    }
+
+}
